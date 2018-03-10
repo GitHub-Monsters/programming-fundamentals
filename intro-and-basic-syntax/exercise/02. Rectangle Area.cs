14d9ce3a1c6e531fdee18653c081e8006084f06f
kokopavlov <kpavlov@hotmail.com>
2018-03-10 18:14:40 +0200

    insterted comments in problem nr. 2

--- intro-and-basic-syntax/exercise/02. Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/02. Rectangle Area.cs
@@ -6,9 +6,14 @@ namespace _02._Rectangle_Area
     {
         static void Main(string[] args)
         {
+			//declare variables
+			//declare the rectangle's width
             double width = double.Parse(Console.ReadLine());
+			//declare the rectangle's heigth
             double heigth = double.Parse(Console.ReadLine());
+			//multiply the width by the heigth to get the rectangle's area and store in a new variable
             double area = width * heigth;
+			//write the rectangle's area on the console with two digits after the decimal sign
             Console.WriteLine($"{area:f2}");
         }
     }
