9eeaaa08110e492b3ec06c066d912a83b5c92eeb
seldadg <seldadg@gmail.com>
2018-03-18 23:30:47 +0200

    changed variable name in Problem 2

--- intro-and-basic-syntax/exercise/02. Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/02. Rectangle Area.cs
@@ -13,7 +13,7 @@ namespace _02._Rectangle_Area
 			//declare the rectangle's heigth
             double heigth = double.Parse(Console.ReadLine());
 			//multiply the width by the heigth to get the rectangle's area and store in a new variable
-            double area = width * heigth;
+            double rectangleArea = width * heigth;
 			//write the rectangle's area on the console with two digits after the decimal sign
             Console.WriteLine($"{area:f2}");
         }
